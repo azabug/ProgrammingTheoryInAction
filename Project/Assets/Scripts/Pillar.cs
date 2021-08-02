4fd04c8d93b0b1067304dda4d1a8292a115e2323
azabug <azabug@suiteaz.com>
2021-08-02 22:07:39 +1000

    Initial Commit
    Four Pillars

---
+++ Project/Assets/Scripts/Pillar.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillar : MonoBehaviour
+{
+    protected string objectType = "Pillar";
+    protected Vector3 objectScale = new Vector3(1, 2, 1);
+}
